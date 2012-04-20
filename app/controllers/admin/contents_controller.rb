6e5c4e7a91b731b2cb45a7288cdf00e2aba8727f
-_- Zaba -_- <bsilva@korewa.com.br>
2012-04-20 17:09:43 -0300

    Ajustes no Conteudo Iframe

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -15,6 +15,7 @@ class Admin::ContentsController < ApplicationController
                               :theme_advanced_buttons3 => %w{ tablecontrols | hr removeformat visualaid | sub sup | charmap emotions iespell media advhr | print | ltr rtl | fullscreen},
                               :theme_advanced_buttons4 => %w{ insertlayer moveforward movebackward absolute | styleprops | cite abbr acronym del ins attribs | visualchars nonbreaking pagebreak},
                               #:plugins => %w{ table fullscreen }
+                              :extended_valid_elements => "iframe[src|width|height|name|align]",
                               :plugins => %w{ safari pagebreak style layer table save advhr advimage advlink emotions iespell inlinepopups insertdatetime preview media searchreplace print contextmenu paste directionality fullscreen noneditable visualchars nonbreaking xhtmlxtras template},
                               :width =>"100%",
                             	:height =>"400"
