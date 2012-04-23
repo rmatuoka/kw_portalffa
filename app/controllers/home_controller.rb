a2d1cd886b1313676b7ce8c885c618494b58f104
Rafael Matuoka <rmatuoka@korewa.com.br>
2012-04-23 11:58:28 -0300

    ajustes gsub

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,25 +25,25 @@ class HomeController < ApplicationController
         @WebModules = @Template.first.webmodules.all_published
     
         @WebModules.each do |modulo|
-
           case modulo.position
             when 1
               @Position1 = "<div style='width: 345px; height: 341px'>"+modulo.module_key+"</div>"
             when 2
               @Position2 = modulo.webpart.encoding
               @Dados = Content.find(modulo.module_key)
-              @Position2.gsub!("[Title]",@Dados.name)
-              @Position2.gsub!("[Description]", @Dados.summary)
-            when 3             
+              @Position2 = @Position2.gsub("[Title]",@Dados.name)
+              @Position2 = @Position2.gsub("[Description]", @Dados.summary)
+            when 3         
               @Position3 = modulo.webpart.encoding
               @Dados = Content.find(modulo.module_key)
-              @Position3.gsub!("[Title]",@Dados.name)
-              @Position3.gsub!("[Description]", @Dados.summary)
+              @Position3 = @Position3.gsub("[Title]",@Dados.name)
+              @Position3 = @Position3.gsub("[Description]", @Dados.summary)
             when 4             
               @Position4 = modulo.webpart.encoding
               @Dados = Content.find(modulo.module_key)
-              @Position4.gsub!("[Title]",@Dados.name)
-              @Position4.gsub!("[Description]", @Dados.summary)
+              @Position4.gsub("[Title]",@Dados.name)
+              @Position4.gsub("[Description]", @Dados.summary)
+              
             when 5
               @Position5 = modulo
             when 6
