05be7ebc96337e4fda94215ccea17254839f0cd9
-_- Zaba -_- <bsilva@korewa.com.br>
2012-05-08 22:06:49 -0300

    Ajustes Modulo Vendas

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20120426174812) do
-=======
-ActiveRecord::Schema.define(:version => 20120425224553) do
->>>>>>> Zaba
 
   create_table "banners", :force => true do |t|
     t.string   "name"
