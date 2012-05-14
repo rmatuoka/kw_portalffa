125242623dd6ce0ea8285f04e1aced71e09ad0e6
-_- Zaba -_- <bsilva@korewa.com.br>
2012-05-14 13:46:30 -0300

    Ajustes Validação de Usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,21 @@ class User < ActiveRecord::Base
   has_many :carts
   has_many :orders
   
-  
   validates_presence_of :name
-  validates_length_of :name, :minimum => 5
+  validates_exclusion_of :name, :in => I18n.t('system_record.name')
   validates_presence_of :cpf
+  validates_exclusion_of :cpf, :in => I18n.t('system_record.cpf')  
   validates_uniqueness_of :cpf
   validates_presence_of :rg
+  validates_exclusion_of :rg, :in => I18n.t('system_record.rg')  
   validates_presence_of :cidade
+  validates_exclusion_of :cidade, :in => I18n.t('system_record.cidade')  
   validates_presence_of :estado
+  validates_exclusion_of :estado, :in => I18n.t('system_record.estado')
   validates_presence_of :email
+  validates_exclusion_of :email, :in => I18n.t('system_record.email')
   validates_presence_of :date_of_birth
-
+  validate :check_over_18
   
   attr_writer :role
   after_save :define_role
@@ -45,5 +49,15 @@ class User < ActiveRecord::Base
   def deliver_password_reset_instructions!
     reset_perishable_token!
   end
+  
+  def age(as_at = Time.now)
+    as_at = as_at.utc.to_date if as_at.respond_to?(:utc)
+    as_at.year - date_of_birth.year - ((as_at.month > date_of_birth.month || (as_at.month == date_of_birth.month && as_at.day >= date_of_birth.day)) ? 0 : 1)
+  end
+  
+  def check_over_18
+    errors.add(:date_of_birth, I18n.t("activerecord.errors.messages.check_over_18")) if age < 18
+  end
+  
 end
 
