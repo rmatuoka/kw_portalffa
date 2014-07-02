404f48cf2fb2a4c2ea300f4ac34af86297a947c7
-== Zaba ==- <bsilva@korewa.com.br>
2014-07-02 11:55:31 -0300

    Ajustes ingressos

--- app/controllers/ingressos/home_controller.rb
+++ app/controllers/ingressos/home_controller.rb
@@ -7,7 +7,11 @@ class Ingressos::HomeController < ApplicationController
   before_filter :load_menu  
   
   def index
-    @ticket_homes = TicketHome.all_active
+    @ticket_homes = TicketHome.all_active  
+    @tickets = Ticket.all(
+                          :conditions => ['active = 1 and published = 1 and ticket_date >= curdate() + 1'],
+                          :include => [:ticket_category, :ticket_type],
+                          :order => 'ticket_category_id, ticket_date, ticket_type_id')
     create_template(4)
   end
 
